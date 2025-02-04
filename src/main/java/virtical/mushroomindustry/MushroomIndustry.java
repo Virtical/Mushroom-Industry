db7a43d1888867066833c3003085effd4d508f64
Virtical <138823554+Virtical@users.noreply.github.com>
2025-02-04 16:59:42 +0500

    Create project

---
+++ src/main/java/virtical/mushroomindustry/MushroomIndustry.java
@@ -0,0 +1,15 @@
+package virtical.mushroomindustry;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MushroomIndustry implements ModInitializer {
+	public static final String MOD_ID = "mushroom-industry";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/virtical/mushroomindustry/MushroomIndustryDataGenerator.java
@@ -0,0 +1,11 @@
+package virtical.mushroomindustry;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MushroomIndustryDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
